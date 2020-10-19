acf259aee3941ff73bf354a8334d850335ceaca8
dominikus1993 <dominikus1910@gmail.com>
2020-10-19 19:57:43 +0200

    init HackerNews Bot

---
+++ tests/DevNews.HackerNews.UnitTests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using System;
+using Xunit;
+
+namespace DevNews.HackerNews.UnitTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+        }
+    }
+}
