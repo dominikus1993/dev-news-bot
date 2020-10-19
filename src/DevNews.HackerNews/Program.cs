fd017968d5dca432d1dfbe9b3ea723a97fa289e7
dominikus1993 <dominikus1910@gmail.com>
2020-10-19 20:31:14 +0200

    small changes

--- src/DevNews.HackerNews/Program.cs
+++ src/DevNews.HackerNews/Program.cs
@@ -1,11 +1,26 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HtmlAgilityPack;
 
 namespace DevNews.HackerNews
 {
     class Program
     {
-        static void Main(string[] args)
+        public static async IAsyncEnumerable<string> A()
         {
+            var html = new HtmlWeb();
+            const string url = "https://news.ycombinator.com/";
+            var a = await html.LoadFromWebAsync(url);
+            var b = a.DocumentNode.SelectNodes("//*[@class=\"athing\"]");
+            b.FindFirst()
+            
+            yield break;
+        }
+        static async Task Main(string[] args)
+        {
+            await A().ToListAsync();
             Console.WriteLine("Hello World!");
         }
     }
