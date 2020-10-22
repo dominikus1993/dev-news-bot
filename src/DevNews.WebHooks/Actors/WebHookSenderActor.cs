95c32e3f0ef70a553bd20e175b406521b4989356
dominikus1993 <dominikus1910@gmail.com>
2020-10-22 20:43:59 +0200

    init actor

---
+++ src/DevNews.WebHooks/Actors/WebHookSenderActor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Akka.Actor;
+using DevNews.WebHooks.Application.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DevNews.WebHooks.Actors
+{
+    public class WebHookSenderActor : ReceiveActor
+    {
+        private IWebHookNotifier _webHookNotifier;
+        public WebHookSenderActor(IServiceProvider sp)
+        {
+            _webHookNotifier = sp.GetService<IWebHookNotifier>();
+        }
+    }
+}
--- src/DevNews.WebHooks/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace DevNews.WebHooks
-{
-    public class Class1
-    {
-    }
-}
