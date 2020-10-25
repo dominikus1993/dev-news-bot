e6e378a328017a673ee91ac6427665a56f45c09e
dominikus1993 <dominikus1910@gmail.com>
2020-10-25 16:03:59 +0100

    small changes

---
+++ src/DevNews.Application/HackerNews/Actors/HackerNewsParserActor.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Akka.Actor;
+using DevNews.Application.HackerNews.Servies;
+
+namespace DevNews.Application.HackerNews.Actors
+{
+    public partial class HackerNewsParserActor
+    {
+        public class ParseNewHackerNewsArticles
+        {
+            public static ParseNewHackerNewsArticles Instance = new ParseNewHackerNewsArticles();
+        }    
+    }
+    
+    public partial class HackerNewsParserActor : ReceiveActor
+    {
+        private IHackerNewsParser _hackerNewsParser;
+        public HackerNewsParserActor(IServiceProvider sp, IHackerNewsParser hackerNewsParser)
+        {
+            _hackerNewsParser = hackerNewsParser;
+            Ready();
+        }
+
+        private void Ready()
+        {
+            ReceiveAsync<ParseNewHackerNewsArticles>(async msg =>
+            {
+                var result = _hackerNewsParser.Parse();
+            });
+        }
+    }
+}
