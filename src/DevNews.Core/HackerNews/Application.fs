bc06126f3e989bafc15197941baae8e1e6f4a378
dominikus1993 <dominikus1910@gmail.com>
2020-11-03 21:01:32 +0100

    small changes

--- src/DevNews.Core/HackerNews/Application.fs
+++ src/DevNews.Core/HackerNews/Application.fs
@@ -1,14 +1,37 @@
 namespace DevNews.Core.HackerNews
 
-open System.Collections.Generic
-open System.Threading.Tasks
-open DevNews.Core.Model
 
 module Repositories =
+    open System.Collections.Generic
+    open System.Threading.Tasks
+    open DevNews.Core.Model
+   
     type IHackerNewsRepository =
         abstract member Exists: articles: Article seq -> IAsyncEnumerable<struct (Article * ArticleExistence)>
         abstract member InsertMany: articles: Article seq -> Task
 
+
+module Services =
+    open FSharp.Control
+    open HtmlAgilityPack
+    open System.Linq
+
+    [<Literal>]
+    let private HackerNewsUrl = "https://news.ycombinator.com/"
+
+    type ArticleDto = { Title: string; Link: string }
+
+    type ParseHackerNewsArticles = unit -> IAsyncEnumerable<ArticleDto>
+
+    let parse () =
+        asyncSeq {
+            let html = HtmlWeb()
+            let! document = html.LoadFromWebAsync(HackerNewsUrl) |> Async.AwaitTask
+            let nodes = document.DocumentNode.SelectNodes("//*[@class=\"storylink\"]").Select(fun e -> {| link = e.GetAttributeValue("href", null); title = e.InnerText |})
+            for node in nodes do
+                yield { Title = node.title; Link = node.link }
+        }
+        
 
 module UseCases = 
     let parseArticlesAndNotify = 
