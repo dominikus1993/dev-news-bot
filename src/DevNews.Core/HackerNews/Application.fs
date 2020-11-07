ff09c1e118272e4385124a6c9955f4da2f3ccb15
dominikus1993 <dominikus1910@gmail.com>
2020-11-07 21:10:45 +0100

    add parseArticlesAndNotify

--- src/DevNews.Core/HackerNews/Application.fs
+++ src/DevNews.Core/HackerNews/Application.fs
@@ -1,15 +1,16 @@
 namespace DevNews.Core.HackerNews
 open System.Threading.Tasks
+open DevNews.Core.Model
 
 
 module Repositories =
     open System.Collections.Generic
     open System.Threading.Tasks
     open DevNews.Core.Model
-   
-    type IHackerNewsRepository =
-        abstract member Exists: articles: Article seq -> IAsyncEnumerable<struct (Article * ArticleExistence)>
-        abstract member InsertMany: articles: Article seq -> Task
+    
+    type Exists = Article -> Async<Option<Article>>
+    
+    type InsertMany = Article seq -> Async<unit>
 
 
 module Services =
@@ -21,35 +22,43 @@ module Services =
     [<Literal>]
     let private HackerNewsUrl = "https://news.ycombinator.com/"
 
-    type ArticleDto = { Title: string; Link: string }
+    type ParseHackerNewsArticles = unit -> AsyncSeq<Article>
 
-    type ParseHackerNewsArticles = unit -> IAsyncEnumerable<ArticleDto>
-
-    type Notify = ArticleDto seq -> Task
-
-    let private parseHtml() =
-        async {
-            let html = HtmlWeb()
-            let! document = html.LoadFromWebAsync(HackerNewsUrl) |> Async.AwaitTask
-            return query {
-                for node in document.DocumentNode.SelectNodes("//*[@class=\"storylink\"]") do
-                    select {| link = node.GetAttributeValue("href", null); title = node.InnerText |}
-            } 
-        } 
+    type Notify = Article seq -> Async<unit>
+    
+    type GetNewArticles  = unit -> AsyncSeq<Article>
 
     let parse () =
         asyncSeq {
-            let! nodes = parseHtml()
+            let html = HtmlWeb()
+            let! document = html.LoadFromWebAsync(HackerNewsUrl) |> Async.AwaitTask
+            let nodes = query {
+                for node in document.DocumentNode.SelectNodes("//*[@class=\"storylink\"]") do
+                    select struct {| link = node.GetAttributeValue("href", null); title = node.InnerText |}
+            }
+            
             for node in nodes do
                 yield { Title = node.title; Link = node.link }
         } 
         
-
+    let getNewArticles (parse: ParseHackerNewsArticles) (getIfExists: Repositories.Exists) =
+        parse()
+            |> AsyncSeq.map(fun x -> { Title = x.Title; Link = x.Link })
+            |> AsyncSeq.mapAsyncParallel(fun x -> getIfExists(x))
+            |> AsyncSeq.choose(id)
+   
 module UseCases = 
     open FSharp.Control
-
-    let parseArticlesAndNotify(parse: Services.ParseHackerNewsArticles) (repo: Repositories.IHackerNewsRepository) (notifier) = 
-        parse()
+    
+    
+    
+    let parseArticlesAndNotify(getNewArticles: Services.GetNewArticles) (insertMany: Repositories.InsertMany)(notify: Services.Notify) =
+        async {
+            let! articles = getNewArticles() |> AsyncSeq.toArrayAsync
+            do! insertMany articles
+            do! notify articles
+            return ();
+        }
 
 
 
