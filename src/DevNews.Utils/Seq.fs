1c9c1b34f04166f3c9b8f98a47dbdb2cce0db666
dominikus1993 <dominikus1910@gmail.com>
2020-11-12 16:43:39 +0100

    add Seq.paged

--- src/DevNews.Utils/Seq.fs
+++ src/DevNews.Utils/Seq.fs
@@ -1,8 +1,17 @@
 namespace DevNews.Utils
 
 module Seq =
-    let paged (pageSize: int)(sequence: _ seq) =
+    open System.Collections.Generic
+    open System.Linq
+    
+    let paged (pageSize: int)(sequence: _ seq) : _ seq seq =
         seq {
-            yield sequence
+            use enumerator = sequence.GetEnumerator()
+            while enumerator.MoveNext() do
+                let current = List<_>()
+                current.Add(enumerator.Current)
+                while (current.Count < pageSize && enumerator.MoveNext()) do
+                    current.Add(enumerator.Current)
+                yield current.AsEnumerable()
         }
 
--- tests/DevNews.UnitTests/SeqTests.fs
+++ tests/DevNews.UnitTests/SeqTests.fs
@@ -1,6 +1,18 @@
 module DevNews.UnitTests.SeqTests
 open Xunit
-open 
+open DevNews.Utils
+open FsUnit.Xunit
+
 [<Fact>]
-let ``My test`` () =
-    Assert.True(true)
+let ``Test empty sequence`` () =
+    let emptySeq: _ seq = Seq.empty
+    let subject = emptySeq |> Seq.paged 10
+    subject |> should be Empty
+    
+    
+[<Fact>]
+let ``Test sequence`` () =
+    let emptySeq: _ seq = seq [|1;2;3;4;5;|]
+    let subject = emptySeq |> Seq.paged 2 |> Seq.map(Seq.toArray) |> Seq.toArray
+    let expected = [|[|1;2|]; [|3;4|]; [|5|]|]
+    subject |> should equal  expected
