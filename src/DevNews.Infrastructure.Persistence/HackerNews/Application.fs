2757e411b5dbcc233f882466dc25b0c64c10156b
dominikus1993 <dominikus1910@gmail.com>
2020-11-16 18:56:24 +0100

    add repo implementation

--- src/DevNews.Infrastructure.Persistence/HackerNews/Application.fs
+++ src/DevNews.Infrastructure.Persistence/HackerNews/Application.fs
@@ -25,3 +25,15 @@ module Repositories =
             let writeMode = art |> Seq.map(fun x -> InsertOneModel<MongoArticle>({ Link = x.Link; Title = x.Title }) :>  WriteModel<MongoArticle>)
             do! col.BulkWriteAsync(writeMode) |> Async.AwaitTask |> Async.Ignore
         }
+        
+    let private getDb(client: MongoClient) =
+        client.GetDatabase("HackerNews")
+    
+    let private getCollection(db: IMongoDatabase) =
+        db.GetCollection<MongoArticle>("articles")
+        
+    type MongoHackerNewsRepository(client: MongoClient) =
+        interface IHackerNewsRepository with
+            member this.Exists = client |> getDb |> getCollection |> checkExistence
+            
+            member this.InsertMany = client |> getDb |> getCollection |> insertArticles
