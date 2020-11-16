ad55294dd9718f0da9104b0a3858ceb2fd8a8e13
dominikus1993 <dominikus1910@gmail.com>
2020-11-16 18:52:47 +0100

    add repo functions

--- src/DevNews.Infrastructure.Persistence/HackerNews/Application.fs
+++ src/DevNews.Infrastructure.Persistence/HackerNews/Application.fs
@@ -9,5 +9,19 @@ module Repositories =
     
     let checkExistence (col: IMongoCollection<MongoArticle>) (art: Article) =
         async {
-            return! col.AsQueryable().AnyAsync(fun x -> x.Title = art.Title) |> Async.AwaitTask
+            let exists =
+                query {
+                    for article in col.AsQueryable() do
+                        exists (article.Title = art.Title)
+                }
+            if exists then
+                return None
+            else
+                return Some(art)
+        }
+        
+    let insertArticles (col: IMongoCollection<MongoArticle>) (art: Article seq) =
+        async {
+            let writeMode = art |> Seq.map(fun x -> InsertOneModel<MongoArticle>({ Link = x.Link; Title = x.Title }) :>  WriteModel<MongoArticle>)
+            do! col.BulkWriteAsync(writeMode) |> Async.AwaitTask |> Async.Ignore
         }
