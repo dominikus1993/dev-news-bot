08658a4a560bbc4c99e178e29e359847becfdf3d
dominikus1993 <dominikus1910@gmail.com>
2020-11-24 12:24:25 +0100

    add unit test project

--- src/DevNews.Core/Service.fs
+++ src/DevNews.Core/Service.fs
@@ -6,10 +6,18 @@ open DevNews.Utils
 module Service =
     open DevNews.Core.Model
     open FSharp.Control
+   
+    type ParseArticles = unit -> AsyncSeq<Article>
+    type ProvideNewArticles = unit -> AsyncSeq<Article>
     
-    type ProvideArticles = unit -> AsyncSeq<Article>
-
-    type Notify = Article seq -> Async<unit>
-
-    type GetNewArticles = unit -> Async<Option<Article seq>>
-
+    let private getArticleIfNotExists(checkArticleExistence: CheckArticleExistence)(article: Article) =
+        async {
+            let! exists = checkArticleExistence(article)
+            return if exists then None else Some(article)
+        }
+    
+    let provideNewArticles (parseArticles: ParseArticles)(checkArticleExistence: CheckArticleExistence)() =
+        parseArticles()
+            |> AsyncSeq.mapAsyncParallel(getArticleIfNotExists(checkArticleExistence))
+            |> AsyncSeq.choose(id)
+        
--- src/DevNews.Core/UseCase.fs
+++ src/DevNews.Core/UseCase.fs
@@ -2,6 +2,7 @@ namespace DevNews.Core
 
 module UseCase =
     open System
+    open DevNews.Core.Service
     
     type ParseHackerNewsArticlesAndNotify = unit -> Async<unit>
     type CheckPossibilityOfParsingArticles = DateTime -> Async<bool>
