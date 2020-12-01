9a1b1e8a688edb67b4dec94648403428364d758e
dominikus1993 <dominikus1910@gmail.com>
2020-12-01 20:27:39 +0100

    add dotnetomaniak parser

--- src/DevNews.Infrastructure.Parsers/Dotnetomaniak/DotnetomaniakArticlesParser.cs
+++ src/DevNews.Infrastructure.Parsers/Dotnetomaniak/DotnetomaniakArticlesParser.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using DevNews.Core.Abstractions;
@@ -9,18 +10,29 @@ namespace DevNews.Infrastructure.Parsers.Dotnetomaniak
     public class DotnetomaniakArticlesParser : IArticlesParser
     {
         public const string DotnetoManiakUrl = "https://dotnetomaniak.pl/";
+        private static readonly Uri DotnetoManiakUri = new(DotnetoManiakUrl);
         public async IAsyncEnumerable<Article> Parse()
         {
             var html = new HtmlWeb();
             var document = await html.LoadFromWebAsync(DotnetoManiakUrl);
-            
-            var nodes = document.DocumentNode.SelectNodes("//*[@class=\"storylink\"]")
-                .Select(node => new Article(node.InnerText, node.GetAttributeValue("href", null)));
-            
+
+            var nodes = document.DocumentNode.SelectNodes("//*[@class=\"article\"]")
+                .Select(x => x.ChildNodes).Select(static n => CreateArticle(n));
+
             foreach (var article in nodes)
             {
                 yield return article;
             }
+        }
+
+        private static Article CreateArticle(HtmlNodeCollection nodes)
+        {
+            var titleNode = nodes.First(div => div.HasClass("title"));
+            var href = titleNode.ChildNodes.FindFirst("a").GetAttributeValue("href", null);
+            var link = new Uri(DotnetoManiakUri, href);
+            var descElement = nodes.First(div => div.HasClass("description"));
+            var description = descElement.ChildNodes.FindFirst("span").InnerText;
+            return new Article(titleNode.InnerHtml, description, link.AbsoluteUri);
         }
     }
 }
