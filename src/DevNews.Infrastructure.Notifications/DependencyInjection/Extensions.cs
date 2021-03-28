1516008deaef18143d7888ad66239986aa1985cf
dominikus1993 <dominikus1910@gmail.com>
2021-03-28 09:29:31 +0200

    update deps and remove teams notifier

--- src/DevNews.Infrastructure.Notifications/DependencyInjection/Extensions.cs
+++ src/DevNews.Infrastructure.Notifications/DependencyInjection/Extensions.cs
@@ -1,8 +1,5 @@
-using System;
 using DevNews.Core.Abstractions;
 using DevNews.Infrastructure.Notifications.Discord;
-using DevNews.Infrastructure.Notifications.MicrosoftTeams;
-using DevNews.Infrastructure.Notifications.MicrosoftTeams.Model;
 using Discord.Webhook;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -14,7 +11,6 @@ namespace DevNews.Infrastructure.Notifications.DependencyInjection
         public static IServiceCollection AddNotifiers(this IServiceCollection services, IConfiguration configuration)
         {
             AddDiscord(services, configuration);
-            AddMicrosoftTeams(services, configuration);
             return services;
         }
 
@@ -22,21 +18,6 @@ namespace DevNews.Infrastructure.Notifications.DependencyInjection
         {
             services.AddSingleton(_ => new DiscordWebhookClient(configuration.GetConnectionString("Discord")));
             services.AddTransient<INotifier, DiscordWebHookNotifier>();
-            return services;
-        }
-        
-        private static IServiceCollection AddMicrosoftTeams(IServiceCollection services, IConfiguration configuration)
-        {
-            if (bool.TryParse(configuration["MicrosoftTeams:Enabled"], out var enabled) && enabled)
-            {
-                services.AddScoped<ITeamsMessageSerializer, JsonTeamsMessageSerializer>();
-                services.AddTransient<INotifier, MicrosoftTeamsWebHookNotifier>();
-                services.AddHttpClient(MicrosoftTeamsWebHookNotifier.MicrosoftTeamsWebHookNotifierApi, client =>
-                {
-                    client.BaseAddress = new Uri(configuration.GetConnectionString("Teams") ?? throw new ArgumentNullException());
-                });
-            }
-
             return services;
         }
     }
--- src/DevNews.Infrastructure.Notifications/MicrosoftTeams/Model/ITeamsMessageSerializer.cs
+++
@@ -1,9 +0,0 @@
-using System.Threading.Tasks;
-
-namespace DevNews.Infrastructure.Notifications.MicrosoftTeams.Model
-{
-    public interface ITeamsMessageSerializer
-    {
-        ValueTask<string> Serialize(MicrosoftTeamsMessage msg);
-    }
-}
