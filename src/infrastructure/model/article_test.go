6e6d90cbbcee4c01ca1854a60bde0c5860cbaa50
Dominik Kotecki <dominikus1910@gmail.com>
2021-10-31 19:07:07 +0100

    add test for FromArticles function

--- src/infrastructure/model/article_test.go
+++ src/infrastructure/model/article_test.go
@@ -1,1 +1,15 @@
 package model
+
+import (
+	"testing"
+
+	"github.com/dominikus1993/dev-news-bot/src/core/model"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestFromArticles(t *testing.T) {
+	articles := []model.Article{model.NewArticle("sdd", "dasas"), model.NewArticle("sdd", "dasas")}
+	subject := FromArticles(articles)
+	assert.NotNil(t, subject)
+	assert.Len(t, subject, 2)
+}
