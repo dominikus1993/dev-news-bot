1de62e253dbac0b824b7ec2a9dce9ffd6ddd8ebf
Dominik Kotecki <dominikus1910@gmail.com>
2021-11-01 14:50:40 +0100

    add getLink function

--- src/infrastructure/parser/hackernews.go
+++ src/infrastructure/parser/hackernews.go
@@ -2,6 +2,7 @@ package parser
 
 import (
 	"context"
+	"net/url"
 
 	"github.com/dominikus1993/dev-news-bot/src/core/model"
 	"github.com/gocolly/colly/v2"
@@ -18,11 +19,23 @@ func NewHackerNewsArticleParser() *hackerNewsArticleParser {
 	return &hackerNewsArticleParser{}
 }
 
+func getLink(e *colly.HTMLElement) string {
+	link := e.Attr("href")
+	url, _ := url.Parse(link)
+	if url.Scheme == "" {
+		url.Scheme = "https"
+	}
+	if url.Host == "" {
+		url.Host = hackerNewsURL
+	}
+	return url.String()
+}
+
 func (p *hackerNewsArticleParser) Parse(ctx context.Context) ([]model.Article, error) {
 	result := make([]model.Article, 0)
 	c := colly.NewCollector()
 	c.OnHTML(".titlelink", func(e *colly.HTMLElement) {
-		result = append(result, model.NewArticle(e.Text, e.Attr("href")))
+		result = append(result, model.NewArticle(e.Text, getLink(e)))
 	})
 	err := c.Visit(hackerNewsURL)
 	if err != nil {
