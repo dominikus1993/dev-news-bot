8b4a4ac2861b7ecc665841cfc9ac67e4fa387b9e
Dominik.Kotecki <dominikus1910@gmail.com>
2022-03-13 15:16:04 +0000

    use hackernews api instead of html

--- internal/parser/hackernews/parser.go
+++ internal/parser/hackernews/parser.go
@@ -2,48 +2,118 @@ package hackernews
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
-	"net/url"
+	"math/rand"
+	"net/http"
+	"time"
 
 	"github.com/dominikus1993/dev-news-bot/pkg/model"
-	"github.com/gocolly/colly/v2"
 	log "github.com/sirupsen/logrus"
 )
 
-const (
-	hackerNewsURL    string = "news.ycombinator.com"
-	hackerNewsScheme string = "https"
-)
+type hackernewsArticle struct {
+	By          string `json:"by"`
+	Descendants int    `json:"descendants"`
+	ID          int    `json:"id"`
+	Kids        []int  `json:"kids"`
+	Score       int    `json:"score"`
+	Time        int    `json:"time"`
+	Title       string `json:"title"`
+	Type        string `json:"type"`
+	URL         string `json:"url"`
+}
 
 type hackerNewsArticleParser struct {
+	client *http.Client
 }
 
 func NewHackerNewsArticleParser() *hackerNewsArticleParser {
-	return &hackerNewsArticleParser{}
+	return &hackerNewsArticleParser{client: getClient()}
 }
 
-func getLink(e *colly.HTMLElement) string {
-	link := e.Attr("href")
-	url, _ := url.Parse(link)
-	if url.Scheme == "" {
-		url.Scheme = "https"
+func getClient() *http.Client {
+	return &http.Client{
+		Timeout: time.Second * 10,
 	}
-	if url.Host == "" {
-		url.Host = hackerNewsURL
+}
+
+func takeRandomArticesIds(ids []int, take int) []int {
+	if take == 0 {
+		return make([]int, 0)
 	}
-	return url.String()
+	if take >= len(ids) {
+		return ids
+	}
+	r := rand.New(rand.NewSource(time.Now().Unix()))
+	randomIds := make([]int, 0, take)
+	for i := 0; i < take; i++ {
+		index := r.Intn(len(ids))
+		randomIds = append(randomIds, ids[index])
+	}
+
+	return randomIds
+}
+
+func getTopArticlesIds(client *http.Client) ([]int, error) {
+	url := "https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty"
+	req, _ := http.NewRequest("GET", url, nil)
+	req.Header.Set("User-Agent", "dev-news-bot")
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("error while parsing hackernews top articless, status: %s", resp.Status)
+	}
+
+	defer resp.Body.Close()
+	var res []int
+	if err := json.NewDecoder(resp.Body).Decode(&res); err != nil {
+		return nil, err
+	}
+	l := len(res)
+	log.Infof("Parsed %d ids from hackernews", l)
+	return res, nil
+}
+
+func getArticle(id int, client *http.Client) (*hackernewsArticle, error) {
+	url := fmt.Sprintf("https://hacker-news.firebaseio.com/v0/item/%d.json?print=pretty", id)
+	req, _ := http.NewRequest("GET", url, nil)
+	req.Header.Set("User-Agent", "dev-news-bot")
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("error while parsing hackernews article by id: %d, status: %s", id, resp.Status)
+	}
+
+	defer resp.Body.Close()
+	var res hackernewsArticle
+	if err := json.NewDecoder(resp.Body).Decode(&res); err != nil {
+		return nil, err
+	}
+	return &res, nil
 }
 
 func (p *hackerNewsArticleParser) Parse(ctx context.Context) ([]model.Article, error) {
 	result := make([]model.Article, 0)
-	c := colly.NewCollector()
-	c.OnHTML(".titlelink", func(e *colly.HTMLElement) {
-		result = append(result, model.NewArticle(e.Text, getLink(e)))
-	})
-	err := c.Visit(fmt.Sprintf("%s://%s", hackerNewsScheme, hackerNewsURL))
+	ids, err := getTopArticlesIds(p.client)
 	if err != nil {
 		return nil, err
 	}
-	log.WithContext(ctx).WithField("quantity", len(result)).Infoln("Parsed hackernews articles")
+	ids = takeRandomArticesIds(ids, 10)
+	for _, id := range ids {
+		hackerNewsArticle, err := getArticle(id, p.client)
+		if err != nil {
+			log.WithField("id", id).WithError(err).Error("error while parsing article by id")
+			continue
+		}
+		article := model.NewArticle(hackerNewsArticle.Title, hackerNewsArticle.URL)
+		if article.IsValid() {
+			result = append(result, model.NewArticle(hackerNewsArticle.Title, hackerNewsArticle.URL))
+		}
+	}
 	return result, nil
 }
