e28d10b4ab8d346e2b03749f3b02ead14a105cc6
Dominik.Kotecki <dominikus1910@gmail.com>
2022-10-15 15:59:44 +0000

    add echojs parser

---
+++ internal/parser/echojs/parser_test.go
@@ -0,0 +1,20 @@
+package echojs
+
+import (
+	"context"
+	"testing"
+
+	"github.com/dominikus1993/dev-news-bot/internal/common/channels"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestHackerNews(t *testing.T) {
+	parser := NewEechoJsParser()
+	result := parser.Parse(context.TODO())
+	subject := channels.ToSlice(result)
+	assert.NotNil(t, subject)
+	assert.NotEmpty(t, subject)
+	for _, article := range subject {
+		assert.True(t, article.IsValid())
+	}
+}
